709451538946c66e178004bbf7586a63a99f7b9b
sdwalker <sdwalker@wm.edu>
2024-10-22 10:42:28 -0400

    Different browser tabs can now communicate

--- src/client/main.ts
+++ src/client/main.ts
@@ -1,8 +1,8 @@
 import {fromEvent, map, tap, merge, shareReplay} from "rxjs";
+import { sendMessage, serverMessages$ } from "./connection";
 
 const form = document.getElementById("form")!;
-const userMessages$ = fromEvent<FormDataEvent>(form, 'submit');
-userMessages$.pipe(
+const userMessages$ = fromEvent<FormDataEvent>(form, 'submit').pipe(
     tap((e: Event) => e.preventDefault()),
     map((e: Event) => {
         const messageInput: HTMLInputElement = ((e.currentTarget as HTMLFormElement).querySelector('input[name="message"]')!);
@@ -12,8 +12,13 @@ userMessages$.pipe(
     }),
     map((message: string): Message => {
         return {data:message, action:"sent", timestamp: new Date()};
-    })
-).subscribe(message => {
+    }),
+    shareReplay()
+)
+
+const messages$ = merge(userMessages$, serverMessages$);
+
+messages$.subscribe((message: Message) => {
     const newMessage = document.createElement("li");
     newMessage.innerHTML = `
         <div>
@@ -23,4 +28,9 @@ userMessages$.pipe(
     `;
     newMessage.classList.add(message.action);
     document.getElementById("messages")!.appendChild(newMessage);
+});
+
+userMessages$.subscribe((message) => {
+    sendMessage(message);
+    console.log(message)
 })
